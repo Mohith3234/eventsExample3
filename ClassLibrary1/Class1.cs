b54a1314d97a054cec61a66343518c6dd81b0a83
Koninti Mohithkumar Goud <al6127@accionlabs.in>
2024-05-20 21:18:01 +0530

    first commit

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+using System.Runtime.CompilerServices;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        //public delegate void MyDelegate(int x, int y);
+
+        public event Predicate<int> myEvent;
+            
+        public bool RaiseEvent(int x)
+        {
+            if(this.myEvent != null)
+            {
+                bool m= this.myEvent(x);
+                return m;
+            }
+            else
+            {
+                return false;
+            }
+           
+        }
+    }
+}
---
+++ EventsExample3/Program.cs
@@ -0,0 +1,23 @@
+﻿using static System.Console;
+using ClassLibrary1;
+namespace EventsExample3
+{
+     class Program
+    {
+        static void Main()
+        {
+            Class1 class1 = new Class1();
+            class1.myEvent += (int x) =>
+            {
+                return x > 0;
+                
+            };
+          
+            WriteLine(class1.RaiseEvent(10));
+            WriteLine(class1.RaiseEvent(20));
+            WriteLine(class1.RaiseEvent(-2));
+            WriteLine(class1.RaiseEvent(-4));
+            ReadKey();
+        }
+    }
+}
